720cef6b57942015722f94c882613820d214020d
Avneet Singh Virdi <avneetvirdi26@gmail.com>
2022-08-03 02:21:46 +0530

    Header and footer added

---
+++ src/components/Footer.svelte
@@ -0,0 +1,17 @@
+<footer>
+    <div class="copyright">Copyright 2020 Poll Ninja</div>
+</footer>
+
+<style>
+    footer{
+        padding: 40px;
+        text-align: center;
+    }
+    .copyright{
+        border-top: 1px solid #ddd;
+        color: #aaa;
+        display: inline-block;
+        font-size: 14px;
+        padding: 20px;
+    }
+</style>
